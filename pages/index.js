2f179cf83194635696f6cc3961832703cb1a8ad1
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2022-07-28 23:19:39 +0530

    Sanity Database Setup Completed

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,12 @@ import Head from "next/head";
 import Hero from "../components/Hero";
 import Layout from "../components/Layout";
 import Services from "../components/Services";
+import { client } from "../lib/client";
 import css from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ pizzas }) {
+    console.log(pizzas);
+
     return (
         <Layout>
             <div className={css.container}>
@@ -26,3 +29,12 @@ export default function Home() {
         </Layout>
     );
 }
+
+export const getServerSideProps = async () => {
+    const query = '*[_type == "pizza"]';
+    const pizzas = await client.fetch(query);
+
+    return {
+        props: { pizzas },
+    };
+};
